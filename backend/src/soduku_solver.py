5442b5bb3c2add83b27ec100f5930f7faed5befa
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-18 13:12:30 +0100

    init of sodoku generator and deleted main

--- backend/src/soduku_solver.py
+++ backend/src/soduku_solver.py
@@ -9,8 +9,3 @@ class Soduko_Solver:
             for j in range(self.size):
                 return NotImplementedError
                 
-
-if __name__ == "__main__":
-    solver = Soduko_Solver([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    if solver is not None:
-        solver.solve()
