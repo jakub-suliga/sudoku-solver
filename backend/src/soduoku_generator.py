a3a51cab47da1e7e9bba9b684314077099550ab1
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-18 13:18:41 +0100

    Implement generated_sodoku method and add placeholders for validation methods

--- backend/src/soduoku_generator.py
+++ backend/src/soduoku_generator.py
@@ -2,5 +2,13 @@ class sodoku_generaotr:
     def __init__(self):
         pass
     
-    def generated_sodoku():
+    def generated_sodoku(size):
+        result = [["" for _ in range(n)] for _ in range(n)]
+        return result
+        
+    def check_valid_in_row():
         return NotImplementedError
+    
+    def check_valid_in_colum():
+        return NotImplementedError
+        
