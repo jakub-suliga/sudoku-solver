c08ec6f2563176255375bde2060dc33b655e928f
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-18 12:43:59 +0100

    Started with the solve Method

--- backend/src/soduku_solver.py
+++ backend/src/soduku_solver.py
@@ -1,10 +1,14 @@
 
 class Soduko_Solver:
-    def __init__(self, board):
+    def __init__(self, board, size=9):
         self.board = board
+        self.size = size
     
     def solve(self):
-        return self.board
+        for i in range(self.size):
+            for j in range(self.size):
+                return NotImplementedError
+                
 
 if __name__ == "__main__":
     solver = Soduko_Solver([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
