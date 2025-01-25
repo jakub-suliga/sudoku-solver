ccae028922913012e3516df4957749d088538f86
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-25 18:55:10 +0100

    Implement column and box validation methods in Soduko_Solver class

--- src/soduku_solver.py
+++ src/soduku_solver.py
@@ -23,10 +23,18 @@ class Soduko_Solver:
                 return False
     
     def check_valid_in_colum(self, col, num):
-        return NotImplementedError
+        for i in range(self.size):
+            if self.board[i][col] == num:
+                return False
 
     def check_valid_in_box(self, row, col, num):
-        return NotImplementedError
+        start_row = row - row % 3
+        start_col = col - col % 3
+        for i in range(3):
+            for j in range(3):
+                if self.board[i + start_row][j + start_col] == num:
+                    return False
+        return True
     
     def is_valid(self, row, col, num):
         return self.check_valid_in_row(row, num) and self.check_valid_in_colum(col, num) and self.check_valid_in_box(row, col, num)
