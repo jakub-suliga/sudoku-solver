c4f6b2fc5354a241d003969c2591fd669230aca4
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-25 18:48:49 +0100

    Implement recursive solve method and add validation placeholders for rows, columns, and boxes

--- backend/src/soduku_solver.py
+++ backend/src/soduku_solver.py
@@ -7,5 +7,24 @@ class Soduko_Solver:
     def solve(self):
         for i in range(self.size):
             for j in range(self.size):
-                return NotImplementedError
+                if self.board[i][j] == 0:
+                    for num in range(1, 10):
+                        if self.is_valid(i, j, num):
+                            self.board[i][j] = num
+                            if self.solve():
+                                return True
+                            self.board[i][j] = 0
+                    return False
                 
+                
+    def check_valid_in_row():
+        return NotImplementedError
+    
+    def check_valid_in_colum():
+        return NotImplementedError
+
+    def check_valid_in_box():
+        return NotImplementedError
+    
+    def is_valid(self, row, col, num):
+        return self.check_valid_in_row(row, num) and self.check_valid_in_colum(col, num) and self.check_valid_in_box(row, col, num)
