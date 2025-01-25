350739f418a52066012b5b2c9d5e5bb32ea0a442
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-25 19:26:17 +0100

    Refactor SudokuSolver class to implement backtracking algorithm and improve validation methods

--- src/soduku_solver.py
+++ src/soduku_solver.py
@@ -1,42 +1,66 @@
-
-class Soduko_Solver:
+class SudokuSolver:
     def __init__(self, board, size=9):
         self.board = board
         self.size = size
-    
+        self.box_size = int(size ** 0.5)
+        if self.box_size ** 2 != size:
+            raise ValueError("Size must be a perfect square, e.g., 9 for a 9x9 Sudoku.")
+
     def solve(self):
+        """Löst das Sudoku-Raster mittels Backtracking."""
+        empty = self._find_empty()
+        if not empty:
+            return True  # Sudoku ist gelöst
+        row, col = empty
+
+        for num in range(1, self.size + 1):
+            if self.is_valid(row, col, num):
+                self.board[row][col] = num
+                if self.solve():
+                    return True
+                self.board[row][col] = 0  # Rückgängigmachen
+
+        return False  # Keine Lösung gefunden
+
+    def _find_empty(self):
+        """Findet die nächste leere Zelle im Raster. Gibt (row, col) zurück oder None."""
         for i in range(self.size):
             for j in range(self.size):
                 if self.board[i][j] == 0:
-                    for num in range(1, 10):
-                        if self.is_valid(i, j, num):
-                            self.board[i][j] = num
-                            if self.solve():
-                                return True
-                            self.board[i][j] = 0
+                    return (i, j)
+        return None
+
+    def is_valid(self, row, col, num):
+        """Überprüft, ob das Einfügen von num an Position (row, col) gültig ist."""
+        return (self._check_row(row, num) and
+                self._check_column(col, num) and
+                self._check_box(row, col, num))
+
+    def _check_row(self, row, num):
+        """Überprüft, ob num nicht bereits in der Zeile vorhanden ist."""
+        return num not in self.board[row]
+
+    def _check_column(self, col, num):
+        """Überprüft, ob num nicht bereits in der Spalte vorhanden ist."""
+        return all(self.board[i][col] != num for i in range(self.size))
+
+    def _check_box(self, row, col, num):
+        """Überprüft, ob num nicht bereits im entsprechenden 3x3-Box vorhanden ist."""
+        start_row = row - row % self.box_size
+        start_col = col - col % self.box_size
+        for i in range(self.box_size):
+            for j in range(self.box_size):
+                if self.board[start_row + i][start_col + j] == num:
                     return False
-                
-                
-    def check_valid_in_row(self, row, num):
-        for i in range(self.size):
-            if self.board[row][i] == num:
-                return False
-        return True
-    
-    def check_valid_in_colum(self, col, num):
-        for i in range(self.size):
-            if self.board[i][col] == num:
-                return False
         return True
 
-    def check_valid_in_box(self, row, col, num):
-        start_row = row - row % 3
-        start_col = col - col % 3
-        for i in range(3):
-            for j in range(3):
-                if self.board[i + start_row][j + start_col] == num:
-                    return False
-        return True
-    
-    def is_valid(self, row, col, num):
-        return self.check_valid_in_row(row, num) and self.check_valid_in_colum(col, num) and self.check_valid_in_box(row, col, num)
+    def print_board(self):
+        """Gibt das Sudoku-Raster in einem lesbaren Format aus."""
+        for i in range(self.size):
+            if i % self.box_size == 0 and i != 0:
+                print("-" * (self.size * 2 + self.box_size - 1))
+            for j in range(self.size):
+                if j % self.box_size == 0 and j != 0:
+                    print("|", end=" ")
+                print(self.board[i][j] if self.board[i][j] != 0 else ".", end=" ")
+            print()
