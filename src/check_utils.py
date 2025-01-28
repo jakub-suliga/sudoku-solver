25ee6e3e4090d2f963c8f4588f7c8f6d3203f166
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-28 06:32:20 +0100

    Refactor CheckerUtils methods to be static and add parameters for grid size and box size

--- src/check_utils.py
+++ src/check_utils.py
@@ -1,25 +1,28 @@
 class CheckerUtils:
     @staticmethod
-    def is_valid(self, row, col, num, grid):
+    def is_valid(row, col, num, grid, size=9, box_size=3):
         """Checks if placing num at grid[row][col] is valid."""
-        return (self._check_row(row, num, grid) and
-                self._check_column(col, num, grid) and
-                self._check_box(row, col, num, grid))
+        return (CheckerUtils._check_row(row, num, grid) and
+                CheckerUtils._check_column(col, num, grid, size) and
+                CheckerUtils._check_box(row, col, num, grid, box_size))
      
-    def _check_row(self, row, num, grid):
+    @staticmethod
+    def _check_row(row, num, grid):
         """Check if num is not in the given row."""
         return num not in grid[row]
     
-    def _check_column(self, col, num, grid):
+    @staticmethod
+    def _check_column(col, num, grid, size):
         """Check if num is not in the given column."""
-        return all(grid[i][col] != num for i in range(self.size))
+        return all(grid[i][col] != num for i in range(size))
     
-    def _check_box(self, row, col, num, grid):
+    @staticmethod
+    def _check_box(row, col, num, grid, box_size):
         """Check if num is not in the corresponding box."""
-        start_row = row - row % self.box_size
-        start_col = col - col % self.box_size
-        for i in range(self.box_size):
-            for j in range(self.box_size):
+        start_row = row - row % box_size
+        start_col = col - col % box_size
+        for i in range(box_size):
+            for j in range(box_size):
                 if grid[start_row + i][start_col + j] == num:
                     return False
         return True
