34c372d8ad006ea986b5d11ade70af08c6839f9e
Jakub Suliga <j.p.suliga@gmail.com>
2025-01-28 06:32:30 +0100

    Remove redundant validation methods from SudokuGenerator to streamline solution checking process

--- src/soduoku_generator.py
+++ src/soduoku_generator.py
@@ -48,31 +48,7 @@ class SudokuGenerator:
                 if self._has_unique_solution(grid_copy):
                     attempts -= 1
                 else:
-                    grid[i][j] = backup  # Wiederherstellen, da die Lösung nicht eindeutig ist
-    
-    def _is_valid(self, row, col, num, grid):
-        """Checks if placing num at grid[row][col] is valid."""
-        return (self._check_row(row, num, grid) and
-                self._check_column(col, num, grid) and
-                self._check_box(row, col, num, grid))
-    
-    def _check_row(self, row, num, grid):
-        """Check if num is not in the given row."""
-        return num not in grid[row]
-    
-    def _check_column(self, col, num, grid):
-        """Check if num is not in the given column."""
-        return all(grid[i][col] != num for i in range(self.size))
-    
-    def _check_box(self, row, col, num, grid):
-        """Check if num is not in the corresponding box."""
-        start_row = row - row % self.box_size
-        start_col = col - col % self.box_size
-        for i in range(self.box_size):
-            for j in range(self.box_size):
-                if grid[start_row + i][start_col + j] == num:
-                    return False
-        return True
+                    grid[i][j] = backup     
     
     def _has_unique_solution(self, grid):
         """Checks if the given Sudoku grid has a unique solution."""
